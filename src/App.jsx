7fa17ca1af7ea6544bfc420b45ce37d22f0ed116
troitsky <17783875+troitsky@users.noreply.github.com>
2022-09-27 18:31:36 +0300

    wip data fecthing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
+
+  const [quotes, setQuotes] = useState([1,2 ])
+
+  async function getQuotes() {
+    let result = await fetch(`https://quote-garden.herokuapp.com/api/v3/quotes/random`)
+    .then((response) => response.json())
+    // .then(data =>  setQuotes(data.data))
+    return result
+  }
+
+  async function updateQuotes() {
+    console.log(await getQuotes().statusCode)
+    setQuotes(await getQuotes().data)
+  }
+
+  function QuoteBlock() {
+
+  }
+
+  useEffect(() => {
+    updateQuotes()
+    
+  }, [])
 
   return (
     <div className="App">
@@ -11,15 +34,17 @@ function App() {
         <span className="update_icon material-symbols-outlined">autorenew</span>
       </header>
       <main className='main_container'>
-        <h1 className="author_title">Bill Gates</h1>
-        <div className="quote_block">
-          <div className="quote_highlight"></div>
-          <p className="quote_text">“The first rule of any technology used in a business is that automation applied to an efficient operation will magnify the efficiency. The second is that automation applied to an inefficient operation will magnify the inefficiency.”</p>
-        </div>
+        <h1 className="author_title"></h1>
+        {quotes.map(quote => {
+          return (<div className="quote_block">
+            <div className="quote_highlight"></div>
+            <p className="quote_text">{quote.quoteText}</p>
+          </div>)
+        })}
         <div className="info_block">
           <div>
-            <h3 className="author">Bill Gates</h3>
-            <p className="category">Business</p>
+            <h3 className="author"></h3>
+            <p className="category"></p>
           </div>
           <span className="forward_icon material-symbols-outlined">east</span>
           <span class="pointer_icon material-symbols-outlined">touch_app</span>
